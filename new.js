c416901120692a9aa30f300d1ae14c38ace30e79
mero-firstRepository <grgganesh8@gmail.com>
2025-06-06 23:45:55 +0545

    DOM Manipulation

--- new.js
+++ new.js
@@ -1,15 +1,9 @@
-let input = prompt("Enter a number:")
-//using ternary operator
-input%5===0 ? console.log("it is multiple by 5"):console.log("it is not multiple by 5");
- 
-//FOr a given array with marks of students .Find the average marks of the entire class.
-let marks = [85,97,44,37,76,60];
-let total_marks=0;
-for(let i=0;i<marks.length;i++){
-    total_marks = total_marks+marks[i];
+//DOM Manipulation
+let classFirst = document.querySelectorAll(".classFirst");
+console.dir(classFirst);
+let idx = 1;
+for(div of classFirst){
+
+    div.innerText =`unique value ${idx}`;
+    idx++;
 }
-let average_marks = total_marks/marks.length;
-console.log(average_marks);
-//FOr a given array of numbers, print the square of each value usng the forEach loop.
-let array = [1,2,3,4];
-array.forEach((val)=>{console.log(val*val)});
