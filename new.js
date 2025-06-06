8c98ec67a9a0a96d612e4cdebf2ca0aad7d47cc8
mero-firstRepository <grgganesh8@gmail.com>
2025-06-06 12:50:21 +0545

    Average marks

--- new.js
+++ new.js
@@ -1,3 +1,12 @@
 let input = prompt("Enter a number:")
 //using ternary operator
-input%5===0 ? "it is multiple by 5":"it is not multiple by 5";
+input%5===0 ? console.log("it is multiple by 5"):console.log("it is not multiple by 5");
+ 
+//FOr a given array with marks of students .Find the average marks of the entire class.
+let marks = [85,97,44,37,76,60];
+let total_marks=0;
+for(let i=0;i<marks.length;i++){
+    total_marks = total_marks+marks[i];
+}
+let average_marks = total_marks/marks.length;
+console.log(average_marks);
