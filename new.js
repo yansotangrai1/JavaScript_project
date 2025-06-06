631a296c3ee5eb5482448d11109e38f9b84e3220
mero-firstRepository <grgganesh8@gmail.com>
2025-06-06 07:31:13 +0545

    New product

---
+++ new.js
@@ -0,0 +1,11 @@
+const product ={
+     name :"Ball_Pan",
+     original_price : 285,
+     offer_price : 270,
+
+};
+console.log(product);
+console.log(product.name);
+console.log(product["original_price"]);
+product.name ="Pen";
+console.log(product.name);
