74a608950f2e776e6441feca44329b970eef55f8
mero-firstRepository <grgganesh8@gmail.com>
2025-06-07 09:12:09 +0545

    New Button create and Add

--- new.js
+++ new.js
@@ -7,3 +7,11 @@ for(div of classFirst){
     div.innerText =`unique value ${idx}`;
     idx++;
 }
+//Add new button through javaScript as the first element of body
+let newButton = document.createElement("button");
+newButton.textContent = "Click me!";
+console.log(newButton);
+newButton.style.backgroundColor ="red";
+newButton.style.color = "white";
+let body = document.querySelector("body");
+body.before(newButton);
